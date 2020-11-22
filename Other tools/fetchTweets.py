30bcc32ee89ef28d126bf10a2e0127fc0491049c
harshalitalele <harshalitalele@gmail.com>
2020-11-22 02:39:28 -0500

    added john oliver tweets

--- Other tools/fetchTweets.py
+++ Other tools/fetchTweets.py
@@ -13,7 +13,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-tweetuser = "@cnnbrk"
+tweetuser = "@iamjohnoliver"
 tweetCnt = 20
 data = tweepy.Cursor(api.user_timeline, id=tweetuser).items(tweetCnt)
 
